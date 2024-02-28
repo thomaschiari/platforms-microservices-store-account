df7e7fde958b7e831b4be1180e8953a988b5a0e2
thomaschiari <thomaschiari@gmail.com>
2024-02-28 08:19:55 -0300

    Create login in

---
+++ src/main/java/insper/store/account/LoginIn.java
@@ -0,0 +1,11 @@
+package insper.store.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder 
+@Accessors(fluent = true, chain = true)
+public record LoginIn(
+    String email,
+    String password
+) { }
